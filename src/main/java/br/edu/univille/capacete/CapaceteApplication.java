fcaa09146a9949f0f8183b8420cf9a6ba008a5aa
murilott <murilo_teichert@hotmail.com>
2025-04-23 19:19:33 -0300

    projeto criado

---
+++ src/main/java/br/edu/univille/capacete/CapaceteApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univille.capacete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapaceteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapaceteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/univille/capacete/CapaceteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univille.capacete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapaceteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
